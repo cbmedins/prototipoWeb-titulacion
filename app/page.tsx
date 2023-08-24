97068cae2c9ba23c699e1a2bc0e6178be56e4bc0
Cristhian <cbmedins@gmail.com>
2023-08-24 01:28:49 -0500

    inicio

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,7 @@ import Link from "next/link"
 import { nFormatter } from "@/lib/utils";
 
 export default async function Home() {
-  const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/steven-tey/precedent",
-    {
-      ...(process.env.GITHUB_OAUTH_TOKEN && {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      }),
-      // data will revalidate every 24 hours
-      next: { revalidate: 86400 },
-    },
-  )
-    .then((res) => res.json())
-    .catch((e) => console.log(e));
+  
 
     return (
       <>
