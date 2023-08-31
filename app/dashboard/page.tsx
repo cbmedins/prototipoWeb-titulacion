88f44f9b48f9c63956d7d464d48021738fe3ea3f
cristhian <cbmedins@gmail.com>
2023-08-31 03:28:53 -0500

    'sesion'

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Card from "@/components/home/card-form";
 import Balancer from "react-wrap-balancer";
 import { DEPLOY_URL } from "@/lib/constants";
@@ -12,21 +10,44 @@ import { nFormatter } from "@/lib/utils";
 import ContactForm from "@/components/formulario/ContactForm";
 import Cardform from "@/components/home/card-form";
 
-import { useState } from "react";
-import { signOut } from "next-auth/react";
-import { LayoutDashboard, LogOut } from "lucide-react";
-import Popover from "@/components/shared/popover";
-import { Session } from "next-auth";
-export default function card({ session }: { session: Session }) {
-  const { email, image } = session?.user || {};
-  const [openPopover, setOpenPopover] = useState(false);
+export default async function Home() {
+  
 
-  if (!email) return null;
-
-  console.log('Contenido de session:', email); // Agrega este log
-
-
+    return (
+      <>
+        <div>
+          
+          <h1
+            className=" bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent text-4xl font-bold tracking-[-0.02em] animate-fade-up md:text-7xl text-center"
+            style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+          >
+            <Balancer>Sistema Predictivo de Enfermedades Cardiovasculares</Balancer>
+          </h1>
+          <p
+            className="mt-6 animate-fade-up text-center text-gray-800 opacity-0 md:text-xl"
+            style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+          >
+            <Balancer>
+            Proyecto de titulacion: Deteccion temprana de enfermedades cardiovasculares mediante un modelo predictivo basado en tecnicas de Deep Learning. 
+            </Balancer>
+          </p>
+  
+          <p>
+  
+          </p>
+  
+          <div
+            className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
+            style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
+          >
+            
+           
+  
+          </div>
+        </div>
+        
+      
+    </>
+  );
 }
-
-
 
