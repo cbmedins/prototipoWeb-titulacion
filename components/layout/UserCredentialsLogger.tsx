e23bf8ccd0a132d2f1681c1e36213dd9bbe43f8b
cristhian <cbmedins@gmail.com>
2023-08-31 13:32:47 -0500

    inicio

--- components/layout/UserCredentialsLogger.tsx
+++ components/layout/UserCredentialsLogger.tsx
@@ -1,12 +1,14 @@
 import { Session } from "next-auth";
 import { useState, useEffect } from "react";
+import ContactForm from "@/components/formulario/ContactForm"; // Importa el componente ContactForm
+
 
 interface UserCredentialsLoggerProps {
   session: Session | null;
 }
 
 const UserCredentialsLogger: React.FC<UserCredentialsLoggerProps> = ({ session }) => {
-  const { email } = session?.user || {};
+  const email = session?.user?.email; // Accede a la propiedad 'email' de session.user si existe
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -20,7 +22,11 @@ const UserCredentialsLogger: React.FC<UserCredentialsLoggerProps> = ({ session }
     return null; // O cualquier contenido de carga que desees
   }
 
-  return null;
+  return (
+    <div>
+      {email && <ContactForm email={email} />} {/* Renderiza ContactForm solo si email existe */}
+    </div>
+  );
 };
 
 export default UserCredentialsLogger;
